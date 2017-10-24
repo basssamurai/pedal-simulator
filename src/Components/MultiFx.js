762db82751235303ef7b6ca5ab50afa55ea204c0
Christian <xianlanoue@gmail.com>
2017-10-24 14:19:17 -0400

    initial commit, imported tone.js, created multifx component

---
+++ src/Components/MultiFx.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import Tone from 'tone'
+
+
+class MultiFx extends Component {
+  constructor() {
+    super()
+    this.state = {
+      signal: ,
+      distortion: ,
+      filter: ,
+      delay: , 
+    }
+  }
+}
+
+export default MultiFx
