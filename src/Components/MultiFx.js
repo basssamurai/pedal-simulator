daf20f3c61f9374de3002dea73c183c83f7a158b
Christian <xianlanoue@gmail.com>
2017-10-26 08:56:39 -0400

    sound working

--- src/Components/MultiFx.js
+++ src/Components/MultiFx.js
@@ -1,17 +1,40 @@
 import React, {Component} from 'react'
 import Tone from 'tone'
 
+var signal = new Tone.Synth ({
+  frequency: 440,
+  oscillator: {type: 'square'},
+  envelope  : {
+    attack  : 0.005 ,
+    decay  : 0.1 ,
+    sustain  : 0.3 ,
+    release  : 1
+  }
+})
 
 class MultiFx extends Component {
   constructor() {
     super()
     this.state = {
-      signal: ,
-      distortion: ,
-      filter: ,
-      delay: , 
+      sound: signal,
+      effects: [],
+      connected: false
     }
   }
+  // to apply FX
+  // synth.chain(...this.state.effects)
+
+  connectToMaster() {
+    this.state.sound.toMaster()
+  }
+
+    render() {
+      this.connectToMaster()
+      this.state.sound.triggerAttackRelease('C4', '1n')
+      return(
+        "boop"
+      )
+    }
 }
 
 export default MultiFx
